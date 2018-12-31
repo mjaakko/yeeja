cf126dd305bb4b476ca88e3d25a3cb0b436bc910
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-01-01 01:52:34 +0200

    Create YeelightConstants class for storing constants related to Yeelight

---
+++ src/main/java/xyz/malkki/yeeja/YeelightConstants.java
@@ -0,0 +1,8 @@
+package xyz.malkki.yeeja;
+
+class YeelightConstants {
+    static final String LINE_BREAK = "\r\n";
+
+    static final String YEELIGHT_ADDRESS = "239.255.255.250";
+    static final int YEELIGHT_PORT = 1982;
+}
