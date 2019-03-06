3bf7dc906bc7df9cfde98e7327a09eb3cc1d0436
Jaakko Malkki <jaakko.malkki234@gmail.com>
2019-03-06 22:53:09 +0200

    Documentation for YeelightCommand API

--- src/main/java/xyz/malkki/yeeja/connection/commands/YeelightCommand.java
+++ src/main/java/xyz/malkki/yeeja/connection/commands/YeelightCommand.java
@@ -4,8 +4,21 @@ import java.util.List;
 import java.util.function.Function;
 
 public abstract class YeelightCommand<R> {
+    /**
+     * Gets the method name.
+     * See Yeelight Inter-Operation Spec for list of possible methods.
+     * @return Method name
+     */
     public abstract String getMethod();
+
+    /**
+     * Gets the command parameters as an array
+     * @return Array of parameters for the command
+     */
     public abstract Object[] getParams();
 
+    /**
+     * @return Function that parses Yeelight command response to a Java object
+     */
     public abstract Function<List<Object>, R> responseParser();
 }
